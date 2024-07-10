5e4c07f09acc424e6265122db7c36bc866ee482e
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-07-10 12:10:20 +0530

    Add commit

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,21 +1,12 @@
-import profile from "../../public/pictures/Designer.png"
-import { HomeIcon } from "@heroicons/react/16/solid";
+import logo from "../../public/pictures/app_icon_64.png"
 
 export default function SideBar(){
     return (
-        <div className="w-1/5 h-[100vh] bg-[#242423] py-8 px-4">
-            <div className="flex flex-col items-center  ">
-                <img className="w-[6em] rounded-full flex "  src={profile} alt="Logo"/>
-                <p className="text-white text-lg py-1 flex ">Noah Johnson</p>
+        <aside className="w-1/5 h-[100vh] bg-[#242423]">
+            <div className="py-4 border-stone-500 border-b-2 flex gap-4 items-center px-5 ">
+                <img className="w-[2.5em]" src={logo} alt="logo" />
+                <h2 className="text-white font-semibold text-lg">Expensio</h2>
             </div>
-           
-           
-           
-
-            <ul className=" mt-8 my-8">
-                <button className="py-3 px-3 text-left w-full text-lg font-semibold text-stone-300  hover:bg-[#333533] rounded-xl hover:text-[#40916c] flex"><HomeIcon className="flex w-[1.4em] mx-2"/>  Home</button>
-            </ul>
-
-        </div>
+        </aside>
     );
 } 
