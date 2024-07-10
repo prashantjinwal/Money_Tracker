9f62cdfc9f69c8e2d22a1373c0761e90718d7e0d
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-07-10 13:13:37 +0530

    SideBar Mapping

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,11 +1,36 @@
-import logo from "../../public/pictures/app_icon_64.png"
+// import { XMarkIcon } from "@heroicons/react/16/solid";
+import Svgs from "../assets/SvgMap";
+import logo from "/pictures/app_icon_64.png"
 
 export default function SideBar(){
+
+    
     return (
-        <aside className="w-1/5 h-[100vh] bg-[#242423]">
+        <aside className="w-1/5 h-[100vh] bg-[#323031]">
             <div className="py-4 border-stone-500 border-b-2 flex gap-4 items-center px-5 ">
                 <img className="w-[2.5em]" src={logo} alt="logo" />
                 <h2 className="text-white font-semibold text-lg">Expensio</h2>
+            </div>
+            
+            
+            <ul className="mt-2">
+
+                {Svgs.map((btn)=>{
+                    let btnStyle = "py-3 px-4 text-left w-full  text-white hover:bg-black";
+
+                    return (
+                    <li className="mt-1" key={btn.id}>
+                        <button className={btnStyle}>{btn.text}</button>
+                    </li>
+                    );
+
+                })}
+
+                
+            </ul>
+
+            <div>
+                {/* <button>{XMarkIcon}</button> */}
             </div>
         </aside>
     );
---
+++ src/assets/SvgMap.js
@@ -0,0 +1,62 @@
+// import transition from "../../public/SVG/transitions.svg"
+// import Scheduled from "../../public/SVG/Scheduled.svg"
+// import OverView from "../../public/SVG/Overview.svg"
+// // import Home from "../../public/SVG/Home.svg"
+// import Debts from "../../public/SVG/Debts.svg"
+// import CarditCard from "../../public/SVG/creditCard.svg"
+// import Charts from "../../public/SVG/Charts.svg"
+// import Calender from "../../public/SVG/Calender.svg"
+// import Budget from "../../public/SVG/Budget.svg"
+// import Account from "../../public/SVG/Account.svg"
+
+
+const Svgs = [
+    {
+        id:1,
+        text:"OverView",
+        // iconVisble:OverView,
+    },
+    {
+        id:2,
+        text:"Transactions",
+        // iconVisble:transition,
+    },
+    {
+        id:3,
+        text:"Scheduled transactions",
+        // iconVisble:Scheduled,
+    },
+    {
+        id:4,
+        text:"Accounts",
+        // iconVisble:Account,
+    },
+    {
+        id:5,
+        text:"Credit cards",
+        // iconVisble:CarditCard,
+    },
+    {
+        id:6,
+        text:"Budgets",
+        // iconVisble:Budget,
+    },
+    {
+        id:7,
+        text:"Debts",
+        // iconVisble:Debts,
+    },
+    {
+        id:8,
+        text:"Charts",
+        // iconVisble:Charts,
+    },
+    {
+        id:9,
+        text:"Calendar",
+        // iconVisble:Calender,
+    },
+]
+
+export default Svgs
+
