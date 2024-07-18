889e1f2e414cd214298c7b7b25fa2a58bf4216b0
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-07-18 18:04:34 +0530

    Test Backend#1

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,37 @@
-import express from express
+import express from "express"
+
+const app = express()
+
+app.get( "/test" ,(req, res)=>{
+
+    const test = [
+        {
+            id: 1,
+            content: "test1"
+        },
+        {
+            id: 2,
+            content: "test2"
+        },
+        {
+            id: 3,
+            content: "test3"
+        },
+        {
+            id: 4,
+            content: "test4"
+        },
+        {
+            id: 5,
+            content: "test5"
+        }
+    ];
+    
+    res.send(test)
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, ()=>{
+    console.log(`Serve at http://localhost:${port}`)
+} )
--- frontEnd/src/Pages/OverView.jsx
+++ frontEnd/src/Pages/OverView.jsx
@@ -1,7 +1,25 @@
-export default function OverView (){
+import {useState} from "react"
+
+const OverView = function  (){
+
+ const [test, settest] = useState();
+
     return (
         <div id="screen">
             <h2>OverView</h2>
+            <h2>TEST : {test.length}</h2>
+            <p>
+                {
+                    test.map((T)=>{
+                        <div key={T.id} >
+                            <p>{T.content}</p>
+                        </div>
+                    })
+                }
+            </p>
+
         </div>
     )
 }
+
+export default OverView
