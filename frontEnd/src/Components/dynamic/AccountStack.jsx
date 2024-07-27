576fad35a88fa941f49824122dd8ed2a9443db4c
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-07-27 12:51:57 +0530

    Add component

--- frontEnd/src/Components/dynamic/AccountStack.jsx
+++ frontEnd/src/Components/dynamic/AccountStack.jsx
@@ -1,10 +1,15 @@
 export default function AccountStack(){
     return (
         <div className="bg-[#323031] w-2/3 py-5 px-6">
-            <p>Account</p>
+            <p className="text-xl">Account</p>
 
-            <div>
-                <p></p>
+            <div className="flex justify-between border-amber-50 border-b-2  py-5">
+                <p>Wallet</p>
+                <p>24232</p>
+            </div>
+            <div className="flex justify-between py-5">
+                <p>Account</p>
+                <p>19232</p>
             </div>
             
         
